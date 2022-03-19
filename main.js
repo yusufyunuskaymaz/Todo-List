ab7ab0e95c330eba39b59be317634c487eb80a87
yusufyunuskaymaz <merthamdiali@gmail.com>
2022-03-19 17:59:23 +0300

    Bitti

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
-// Eventler
+const form = document.querySelector("#todo-form")
+const todoInput = document.querySelector("#todo")
+const todoList = document.querySelector(".list-group")
+const firstCardBody = document.querySelectorAll(".card-body")[0]
+const secondCardBody = document.querySelectorAll(".card-body")[1]
+const filter = document.querySelector("#filter")
+const clearButton = document.querySelector("#clear-todos")
 
-const header = document.querySelector(".card-header");
+eventListener();
 
-const todoInput = document.querySelector("#todo");
-
-todoInput.addEventListener("keyup", changeText)
-
-function changeText(mesela){
-    header.textContent = mesela.target.value
+function eventListener(){ //Tüm eventlistenerler
+ form.addEventListener("submit", addTodo)
 }
+
+function addTodo(e){
+
+    const newTodo = todoInput.value.trim();
+
+    addTodoToUI(newTodo)
+
+
+    e.preventDefault()
+}
+
+function addTodoToUI(newTodo){
+
+    const listItem = document.createElement("li");
+
+    const link = document.createElement("a");
+
+    link.href= "#"
+    link.className = "delete-item"
+    link.innerHTML = "<i class = 'fa fa-remove'></i>"
+
+    listItem.className = "list-group-item d-flex justify-content-between"
+    listItem.appendChild(document.createTextNode(newTodo))
+    listItem.appendChild(link)
+
+    todoList.appendChild(listItem)
+
+
+}
+
